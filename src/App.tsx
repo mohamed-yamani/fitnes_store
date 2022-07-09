de5ca37da96aa82c5c74d9f721b3f5e9aea885df
mohamed-yamani <devforever99@gmail.com>
2022-07-09 14:22:02 +0100

    POPULAR COLLECTIONS

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,35 @@ import axios from "./apis/produit";
 import Carousel from "./components/HomePage/Carousel/carousel";
 import PopularCollections from "./components/HomePage/PopularCollections/PopularCollections";
 
+export type ProduitModel = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: ResultModel[];
+};
+
+export type ResultModel = {
+  id: number;
+  nom: string;
+  ordre: any;
+  photo_principal: string;
+  prix: number;
+  nombre_couleurs: number;
+  couleurs: CouleurModel[];
+  categorie: number;
+  promotion: any;
+  is_new: boolean;
+  favoris: boolean;
+  longeur_par_largeur: number;
+  nombre_photo_carroussel: number;
+};
+
+export type CouleurModel = {
+  id: number;
+  code: string;
+  nom: string;
+};
+
 const App = () => {
   //! fetch api produit
   const [data, error, loading] = useAxios({
@@ -15,6 +44,9 @@ const App = () => {
     method: "GET",
     endpoint: "/api/produit/",
   });
+  const CastedProduitData = data as unknown as ProduitModel;
+  const castedResults = CastedProduitData.results as ResultModel[];
+
   console.log("print products -------------- ");
   console.log(data);
   return (
@@ -23,11 +55,15 @@ const App = () => {
       <Carousel />
       <PopularCollections />
 
-      {!loading && !error && data && <Products />}
+      {!loading && !error && data && (
+        <div>
+          <img src={castedResults[0].photo_principal} alt="product" />
+          <Products />
+        </div>
+      )}
       {!loading && !error && !data && <div>No data</div>}
       {loading && <div>Loading...</div>}
       {!loading && error && <div>Error: {error}</div>}
-      <Products />
     </div>
   );
 };
--- src/components/HomePage/PopularCollections/PopularCollections.tsx
+++ src/components/HomePage/PopularCollections/PopularCollections.tsx
@@ -3,7 +3,13 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import { useMediaQuery } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -35,53 +41,273 @@ export default function PopularCollections() {
   const isMobile = useMediaQuery("(max-width:768px)");
   return (
     <Box sx={{ flexGrow: 1 }}>
+      <Typography variant="h4" gutterBottom align="center">
+        POPULAR COLLECTIONS
+      </Typography>
       <Grid container spacing={1}>
         <Grid item xs={12} md={6} height={isMobile ? "100vw" : "25vw"}>
           <Item>
-            <img
-              src="http://www.adlittle.com/sites/default/files/events/corporate_startup_visual-min.jpg"
-              alt="http://www.adlittle.com/sites/default/files/events/corporate_startup_visual-min.jpg"
-            />
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="http://www.adlittle.com/sites/default/files/events/corporate_startup_visual-min.jpg"
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "13%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">RIGS & RACKS</Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
         <Grid item xs={12} md={3}>
           <Item>
-            <img
-              src="https://149718753.v2.pressablecdn.com/wp-content/uploads/2022/02/Exercise-Bikes-Indoor-Cycling-Equipment-Montreal-1024x768.jpg"
-              alt="https://149718753.v2.pressablecdn.com/wp-content/uploads/2022/02/Exercise-Bikes-Indoor-Cycling-Equipment-Montreal-1024x768.jpg"
-            />
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="https://149718753.v2.pressablecdn.com/wp-content/uploads/2022/02/Exercise-Bikes-Indoor-Cycling-Equipment-Montreal-1024x768.jpg"
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "22%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">HOME GYM</Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
         <Grid item xs={12} md={3}>
           <Item>
-            <img
-              src="https://media.istockphoto.com/photos/brutal-caucasian-bodybuilder-training-chest-in-gym-picture-id876233812?k=20&m=876233812&s=612x612&w=0&h=31CrF8d1WRXxqkto-IemcHC94RcIIpTI7bNAGK5s7bQ="
-              alt="https://media.istockphoto.com/photos/brutal-caucasian-bodybuilder-training-chest-in-gym-picture-id876233812?k=20&m=876233812&s=612x612&w=0&h=31CrF8d1WRXxqkto-IemcHC94RcIIpTI7bNAGK5s7bQ="
-            />
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="https://media.istockphoto.com/photos/brutal-caucasian-bodybuilder-training-chest-in-gym-picture-id876233812?k=20&m=876233812&s=612x612&w=0&h=31CrF8d1WRXxqkto-IemcHC94RcIIpTI7bNAGK5s7bQ="
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "16%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">PLATES</Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
         <Grid item xs={12} md={3}>
           <Item>
-            <img
-              src="https://www.elitefts.com/wp/wp-content/uploads/2012/05/the-vault-cover-blank.jpg"
-              alt="https://www.elitefts.com/wp/wp-content/uploads/2012/05/the-vault-cover-blank.jpg"
-            />
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="https://www.elitefts.com/wp/wp-content/uploads/2012/05/the-vault-cover-blank.jpg"
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "33%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">WEIGHT BENCHES</Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
         <Grid item xs={12} md={3}>
           <Item>
-            <img
-              src="http://cdn.shopify.com/s/files/1/0280/9102/3454/files/Gym_51_mobile_banner_grande.jpg?v=1601726359"
-              alt="http://cdn.shopify.com/s/files/1/0280/9102/3454/files/Gym_51_mobile_banner_grande.jpg?v=1601726359"
-            />
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="http://cdn.shopify.com/s/files/1/0280/9102/3454/files/Gym_51_mobile_banner_grande.jpg?v=160172635"
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "21%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">BARBELLS</Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
         <Grid item xs={12} md={6} height={isMobile ? "100vw" : "25vw"}>
-          <Item>
-            <img
+          {/* <img
               src="https://media.istockphoto.com/photos/caucasian-muscular-man-kneeling-and-clapping-hands-in-front-of-him-picture-id1126509424?k=20&m=1126509424&s=612x612&w=0&h=45MCjJAyNlQJqxEN4Kv9UVMd4VeVt-S7EjbvVC4WoCQ="
               alt="https://media.istockphoto.com/photos/caucasian-muscular-man-kneeling-and-clapping-hands-in-front-of-him-picture-id1126509424?k=20&m=1126509424&s=612x612&w=0&h=45MCjJAyNlQJqxEN4Kv9UVMd4VeVt-S7EjbvVC4WoCQ="
-            />
+            /> */}
+
+          <Item>
+            <Card
+              style={{ height: "100%", width: "100%", backgroundColor: "red" }}
+            >
+              <div style={{ height: "100%", width: "100%" }}>
+                <Card
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    top: 0,
+                  }}
+                >
+                  <div
+                    style={{
+                      position: "relative",
+                      backgroundColor: "green",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <CardMedia
+                      style={{ height: "100%", width: "100%" }}
+                      component="img"
+                      image="https://media.istockphoto.com/photos/caucasian-muscular-man-kneeling-and-clapping-hands-in-front-of-him-picture-id1126509424?k=20&m=1126509424&s=612x612&w=0&h=45MCjJAyNlQJqxEN4Kv9UVMd4VeVt-S7EjbvVC4WoCQ="
+                    />
+                    <Box
+                      style={{
+                        position: "absolute",
+                        color: "white",
+                        bottom: "2%",
+                        left: "25%",
+                        transform: "translateX(-50%)",
+                      }}
+                    >
+                      <Typography variant="h4">
+                        GYMNASTICS & BODYWEIGHT
+                      </Typography>
+                    </Box>
+                  </div>
+                </Card>
+              </div>
+            </Card>
           </Item>
         </Grid>
       </Grid>
